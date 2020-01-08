32f1dd989f417e90ad7574ca3037e5b64ae290db
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-01-08 11:33:26 +0100

    Improve mobile footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../../styles/layouts/Footer.scss";
 
 //fontawesome
@@ -8,24 +9,51 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <p className="footer">
-      &copy;2020{" "}
-      <a href="https://github.com/paulinastefanska/portfolio">
-        <b>Paulina Stefańska</b>
-      </a>
-      <a className="footer_social" href="https://github.com/paulinastefanska">
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      <a
-        className="footer_social"
-        href="https://www.linkedin.com/in/paulina-stefanska"
-      >
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
-      <a className="footer_social" href="mailto:web.paulinastefanska@gmail.com">
-        <FontAwesomeIcon icon={faEnvelope} />
-      </a>
-    </p>
+    <>
+      <ul className="footer">
+        <li>
+          <NavLink exact to="/about" className="footer_menu">
+            O mnie
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/skills" className="footer_menu">
+            Umiejętności
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/portfolio" className="footer_menu">
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/contact" className="footer_menu">
+            Kontakt
+          </NavLink>
+        </li>
+      </ul>
+      <p className="footer_links">
+        &copy;2020{" "}
+        <a href="https://github.com/paulinastefanska/portfolio">
+          <b>Paulina Stefańska</b>
+        </a>
+        <a className="footer_social" href="https://github.com/paulinastefanska">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          className="footer_social"
+          href="https://www.linkedin.com/in/paulina-stefanska"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a
+          className="footer_social footer_social--hidden"
+          href="mailto:web.paulinastefanska@gmail.com"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </p>
+    </>
   );
 };
 
