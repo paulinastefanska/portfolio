4be54b9379cf5201a7bf8dad74314533953bb1f0
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-01-11 21:26:07 +0100

    Add home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "../../styles/pages/HomePage.scss";
+//image
+import photo from "../../images/home-pic.JPG";
 
 const HomePage = () => {
-  return <p>Witaj!</p>;
+  return (
+    <section className="home">
+      <img src={photo} alt="me" className="img"></img>
+      <div className="home_welcome">
+        <span className="home_header">Cześć!</span>
+        <p className="home_text">
+          Nazywam się Paulina Stefańska. Około 1.5 roku temu postanowiłam zostać
+          Front-end Developerem. Pomysł nie wziął się znikąd, kiedyś miałam
+          troszkę styczności z kodem, a przez ostatnie lata współpraca z działem
+          IT utwierdziła mnie w tym przekonaniu. Od roku systematycznie uczę się
+          programowania po stronie front-endu, wiedzę mogę poprzeć licznymi
+          projektami oraz kursami. Aktualnie szukam pierwszej pracy w IT.
+          Chętnie wezmę też udział w projektach open source.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
