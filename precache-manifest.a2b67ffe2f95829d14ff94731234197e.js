ebb1fc37d491967bd6845b5a9c827500f5399ea2
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-01-17 00:46:01 +0100

    Updates

---
+++ precache-manifest.a2b67ffe2f95829d14ff94731234197e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c349877af3676607a689769dcf46c17",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9859469222c2ecaaaaba",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4677a4b2b567d8da1638",
+    "url": "/portfolio/static/css/main.a3b79cf1.chunk.css"
+  },
+  {
+    "revision": "9859469222c2ecaaaaba",
+    "url": "/portfolio/static/js/2.fd9e9aec.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/portfolio/static/js/2.fd9e9aec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4677a4b2b567d8da1638",
+    "url": "/portfolio/static/js/main.695e5552.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "f6e7ce561b36e9b778ca995da8732e8f",
+    "url": "/portfolio/static/media/adminpanel.f6e7ce56.png"
+  },
+  {
+    "revision": "0c48d58d73f2b512496a75c0f6102a9b",
+    "url": "/portfolio/static/media/bulb.0c48d58d.svg"
+  },
+  {
+    "revision": "bf1cbdc86349ccc02d823eed36424c7f",
+    "url": "/portfolio/static/media/circle-yellow.bf1cbdc8.svg"
+  },
+  {
+    "revision": "c82b3b36629acf3658e2a6003e18895d",
+    "url": "/portfolio/static/media/circle.c82b3b36.svg"
+  },
+  {
+    "revision": "280724d4ba7d54ce81c3617fde61c2df",
+    "url": "/portfolio/static/media/commentsapp.280724d4.png"
+  },
+  {
+    "revision": "7f81e0c4b1e579e656de40c2a18cc391",
+    "url": "/portfolio/static/media/firstgame.7f81e0c4.png"
+  },
+  {
+    "revision": "c769c48f93ec5a13864986215e1bf3b6",
+    "url": "/portfolio/static/media/flag.c769c48f.svg"
+  },
+  {
+    "revision": "97c476c1673991b69bd21b75a291dcf5",
+    "url": "/portfolio/static/media/folder.97c476c1.svg"
+  },
+  {
+    "revision": "6af82f9854aab94b3fea26dc32efa3ba",
+    "url": "/portfolio/static/media/home-pic.6af82f98.JPG"
+  },
+  {
+    "revision": "0598eb80fc1a2a4d7ea9b5639d4d6933",
+    "url": "/portfolio/static/media/kanbanboard.0598eb80.png"
+  },
+  {
+    "revision": "ad0b17d5e62b7274abc9eb897518b019",
+    "url": "/portfolio/static/media/phone.ad0b17d5.svg"
+  },
+  {
+    "revision": "66b660d07dc60f1c45aca4e8cdc37e70",
+    "url": "/portfolio/static/media/point.66b660d0.svg"
+  },
+  {
+    "revision": "bfe278ee54bf0efd006cb081df04dd16",
+    "url": "/portfolio/static/media/portfolio.bfe278ee.png"
+  },
+  {
+    "revision": "b10ce284a994985f4b025fd53bb9c49e",
+    "url": "/portfolio/static/media/side-circles.b10ce284.svg"
+  },
+  {
+    "revision": "6b6e25aa588d89a85c6e0ad23c11f691",
+    "url": "/portfolio/static/media/snakegame.6b6e25aa.png"
+  },
+  {
+    "revision": "61ca74206f284b62d048be1c9228d6e2",
+    "url": "/portfolio/static/media/techstore.61ca7420.png"
+  },
+  {
+    "revision": "d4cdc491072c7a8d1d740528fb4d4728",
+    "url": "/portfolio/static/media/todoapp.d4cdc491.png"
+  },
+  {
+    "revision": "dc2c44d8b81392baa6fb80a5413c5889",
+    "url": "/portfolio/static/media/weatherapp.dc2c44d8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2cd2ecb8c2a3d1f0a2aafe96afeccb61.js"
+  "/portfolio/precache-manifest.a2b67ffe2f95829d14ff94731234197e.js"
 );
 
 self.addEventListener('message', (event) => {
