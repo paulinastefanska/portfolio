123565f787f75b7f44c0c04c7d35d2c3dab35fd0
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-01-17 19:08:04 +0100

    Add transition animation

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+//animations
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+//pages
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import SkillsPage from "../pages/SkillsPage";
@@ -8,20 +11,40 @@ import ContactPage from "../pages/ContactPage";
 import NotFoundPage from "../pages/NotFoundPage";
 
 import "../../styles/layouts/Main.scss";
+import "../../styles/animation/PageTransition.scss";
 
-const Page = () => {
-  return (
-    <main className="main">
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/skills" component={SkillsPage} />
-        <Route path="/portfolio" component={PortfolioPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </main>
-  );
-};
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default Page;
+  render() {
+    const { location } = this.props;
+
+    return (
+      <TransitionGroup component="main" className="main">
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 800, exit: 400 }}
+          classNames="fade"
+          mountOnEnter={false}
+          unmountOnExit={true}
+        >
+          <div className="wrapper">
+            <Switch location={location}>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/about" exact component={AboutPage} />
+              <Route path="/skills" exact component={SkillsPage} />
+              <Route path="/portfolio" exact component={PortfolioPage} />
+              <Route path="/contact" exact component={ContactPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    );
+  }
+}
+
+export default withRouter(Main);
